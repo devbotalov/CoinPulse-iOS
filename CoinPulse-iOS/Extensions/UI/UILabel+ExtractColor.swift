7dfd4be74adc45ce3cd7053f933694dcee9437b0
Maksim Botalov <dev.botalov@icloud.com>
2024-10-07 15:58:30 +0500

    Added extension for UILabel

---
+++ CoinPulse-iOS/Extensions/UI/UILabel+ExtractColor.swift
@@ -0,0 +1,14 @@
+//
+//  UILabel+ExtractColor.swift
+//  CoinPulse-iOS
+//
+//  Created by Maksim Botalov on 07.10.2024.
+//
+
+import UIKit
+
+extension UILabel {
+    func extractColor() -> UIColor {
+        return self.textColor
+    }
+}
